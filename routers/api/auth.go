562cf13ef9fd62f4442a6baabcbcd9c583e88472
hucy <hucy@gegolab.com>
2020-11-14 12:06:08 +0800

    1. 增加jwt-go在gin中的验证

---
+++ routers/api/auth.go
@@ -0,0 +1,57 @@
+package api
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/astaxie/beego/validation"
+	"github.com/gin-gonic/gin"
+
+	"github.com/hucongyang/go-demo/models"
+	"github.com/hucongyang/go-demo/pkg/errorCode"
+	"github.com/hucongyang/go-demo/pkg/util"
+)
+
+type auth struct {
+	Username string `valid:"Required; MaxSize(50)"`
+	Password string `valid:"Required; MaxSize(50)"`
+}
+
+// 根据用户名密码获取token秘钥，作为调用凭证
+func GetAuth(c *gin.Context) {
+	username := c.Query("username")
+	password := c.Query("password")
+
+	valid := validation.Validation{}
+	a := auth{Username: username, Password: password}
+	ok, _ := valid.Valid(&a)
+
+	data := make(map[string]interface{})
+	code := errorCode.INVALID_PARAMS
+	if ok {
+		isExist := models.CheckAuth(username, password)
+		if isExist {
+			token, err := util.GenerateToken(username, password)
+			if err != nil {
+				code = errorCode.ERROR_AUTH_TOKEN
+			} else {
+				data["token"] = token
+
+				code = errorCode.SUCCESS
+			}
+
+		} else {
+			code = errorCode.ERROR_AUTH
+		}
+	} else {
+		for _, err := range valid.Errors {
+			log.Println(err.Key, err.Message)
+		}
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"code": code,
+		"msg":  errorCode.GetMessage(code),
+		"data": data,
+	})
+}
--- routers/router.go
+++ routers/router.go
@@ -3,6 +3,8 @@ package routers
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/hucongyang/go-demo/conf"
+	"github.com/hucongyang/go-demo/middleware/jwt"
+	"github.com/hucongyang/go-demo/routers/api"
 	v1 "github.com/hucongyang/go-demo/routers/api/v1"
 )
 
@@ -17,7 +19,12 @@ func InitRouter() *gin.Engine {
 
 	gin.SetMode(conf.Config().RunMode)
 
+	// 获取token凭证
+	router.GET("/auth", api.GetAuth)
+
 	apiGroupV1 := router.Group("/api/v1")
+	// 使用中间件jwt
+	apiGroupV1.Use(jwt.JWT())
 	{
 		// 获取标签列表
 		apiGroupV1.GET("/tags", v1.GetTags)
